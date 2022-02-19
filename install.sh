1490d463a0347f86fe9f5c6d2b3b710586a70cbd
Freyr Ólafsson <gnarlin@gmail.com>
2022-02-19 17:32:18 +0000

    Updated oslist with a contribution to oslist form Jeremy Wakeman

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #
 # file: installer.sh
-# This is osget version 1.4
+# This is osget version 1.5
 # written by Freyr Gunnar Ólafsson
 # This program is licenced under the GPL version 3 or higher (if available)
 
